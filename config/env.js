e91a4f988b52eee12014a8914d08f7b48506f629
Bielseba <ominidev78@gmail.com>
2025-12-10 00:06:04 -0300

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,16 +1,10 @@
 import dotenv from 'dotenv'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+dotenv.config()
 
 export const env = {
-  PORT: process.env.PORT || 4001,
   DATABASE_URL: process.env.DATABASE_URL,
-  NODE_ENV: process.env.NODE_ENV,
   JWT_OPERATOR_SECRET: process.env.JWT_OPERATOR_SECRET,
-  HOUSE_USER_ID: process.env.HOUSE_USER_ID
+  HOUSE_USER_ID: process.env.HOUSE_USER_ID,
+  PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV || 'production'
 }
