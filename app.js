4e6f51f54a0533bb208ce25c3bbb559685f2b6af
Edmilton Pansanato <edmilton.vinicius2@gmail.com>
2025-12-11 18:34:14 -0300

    adicionando treasury no cors

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const allowedOrigins = [
   'https://payg2a.online',
   'https://mutual-fintech-front-end-paas.vercel.app',
   'https://omnigateway.site',
+  'https://treasury-service.omnigateway.site',
 ];
 
 app.use((req, res, next) => {
