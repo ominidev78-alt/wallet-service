e06cd4bb006636822bcb26e8113f66b9e23d18ba
Bielseba <ominidev78@gmail.com>
2025-12-11 02:35:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ const allowedOrigins = [
     'https://ominigateway.com.br',
     'https://admin.ominigateway.com.br',
     'https://payg2a.online',
-    'https://mutual-fintech-front-end-paas.vercel.app'
+    'https://mutual-fintech-front-end-paas.vercel.app',
+    'https://omnigateway.site'
 ]
 
 app.use((req, res, next) => {
